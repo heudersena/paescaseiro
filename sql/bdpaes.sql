100444d8d1dd76570d50c40a8d373e1b1ebf0d91
heudersena <documento_arquivo@hotmail.com>
2017-06-06 18:58:29 -0300

    Versão nova do BD 06-06-2017

--- sql/bdpaes.sql
+++ sql/bdpaes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 01/06/2017 às 19:09
+-- Tempo de geração: 06/06/2017 às 18:57
 -- Versão do servidor: 5.7.18-0ubuntu0.17.04.1
 -- Versão do PHP: 7.0.18-0ubuntu0.17.04.1
 
@@ -42,7 +42,12 @@ INSERT INTO `compra` (`id`, `compra`, `data`, `produto_idproduto`) VALUES
 (2, '11.00', '2017-05-30 00:00:00', 2),
 (3, '6.70', '2017-05-30 00:00:00', 2),
 (4, '9.00', '2017-06-01 00:00:00', 2),
-(5, '12.00', '2017-06-01 13:38:33', 2);
+(7, '20.00', '2017-06-02 07:22:19', 2),
+(8, '57.00', '2017-06-02 06:54:39', 2),
+(9, '47.00', '2017-06-04 10:45:23', 2),
+(10, '43.00', '2017-06-06 07:25:12', 2),
+(11, '50.00', '2017-06-06 17:57:27', 2),
+(12, '21.00', '2017-06-06 18:36:10', 2);
 
 -- --------------------------------------------------------
 
@@ -85,7 +90,11 @@ INSERT INTO `venda` (`idvenda`, `venda_diarias`, `data`, `produto_idproduto`) VA
 (1, '46.00', '2017-05-29 00:00:00', 1),
 (2, '35.00', '2017-05-30 00:00:00', 1),
 (15, '35.00', '2017-05-31 22:45:59', 1),
-(17, '43.00', '2017-06-01 18:54:42', 1);
+(17, '43.00', '2017-06-01 18:54:42', 1),
+(18, '90.00', '2017-06-02 06:54:14', 1),
+(19, '49.00', '2017-06-04 10:44:34', 1),
+(20, '62.00', '2017-06-06 07:24:28', 1),
+(21, '53.00', '2017-06-06 17:57:20', 1);
 
 --
 -- Índices de tabelas apagadas
@@ -119,7 +128,7 @@ ALTER TABLE `venda`
 -- AUTO_INCREMENT de tabela `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de tabela `produto`
 --
@@ -129,7 +138,7 @@ ALTER TABLE `produto`
 -- AUTO_INCREMENT de tabela `venda`
 --
 ALTER TABLE `venda`
-  MODIFY `idvenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `idvenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- Restrições para dumps de tabelas
 --
